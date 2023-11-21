4fe95e11d8b9a50b547eb46c7ca55c29f98309d1
AdznlHakim12 <adzinulhakim04@student.ub.ac.id>
2023-11-21 16:45:26 +0700

    tambah lokasi

--- resources/views/findLocation.blade.php
+++ resources/views/findLocation.blade.php
@@ -73,7 +73,7 @@
                         <p class="font-normal text-gray-700">Location: Klojen</p>
                         <!-- Tambahkan informasi sekolah lainnya sesuai kebutuhan -->
                         <br>
-                        <a href="https://www.google.com/maps" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-400 rounded-lg hover:bg-yellow-500">
+                        <a href="https://maps.app.goo.gl/5vwu4tUG11uzq42S7" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-400 rounded-lg hover:bg-yellow-500">
                             Lihat Lokasi
                             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </a>
